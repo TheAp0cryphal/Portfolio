f975c93614a651759944fd870629f15b9bc431d8
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-10 18:39:39 -0700

    Minor Fixes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,27 +4,7 @@ const About = () => {
       <div class="text-center mb-5">
         <h2>About Me</h2>
       </div>
-      <div class="row align-items-center">
-        <div class="col-lg-5 mb-4 mb-lg-0">
-          <div class="profile-img-container mx-auto" style={{"max-width": "350px"}}>
-            <img
-              src="https://via.placeholder.com/400"
-              alt="Profile"
-              class="img-fluid rounded-circle"
-            />
-          </div>
-        </div>
-        <div class="col-lg-7">
-          <h3 class="mb-3">Who I Am</h3>
-          <p class="lead mb-4">
-            Lorem Ipsum
-          </p>
-          <p class="mb-4">
-            Lorem Ipsum
-          </p>
-
-        </div>
-      </div>
+      <div class="row align-items-center"/>
     </div>
   );
 };
