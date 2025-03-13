85cfa4dffbf3d346e79f48bb67e31374ed90178c
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-12 18:31:58 -0700

    Projects : WIP

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -6,8 +6,15 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "eMosh",
       subtitle: "Computer Vision | Human Computer Interaction",
-      description:
-        "A Web App with Multimodal AI that infers human emotions based on facial expressions and audio activations",
+      description: (
+        <>
+          In human computer interaction, we use facial features and audio arousal to determine the user's current emotional state. We then map it to an appropriate emoji :D
+          <br /><br />
+          ● Utilized FFmpeg, Python to develop a pipeline for capturing peak emotion frames from A/V channels separately
+          <br /> <br />
+          ● Created a multi-processing solution to concurrently process images with dynamic bounding boxes, improving speeds by 3x
+        </>
+      ),
       image: "./assets/eMosh.png",
       tags: ["Python", "Flask", "Node.js", "FFmpeg", "PyTorch"],
       demo: "#",
@@ -17,8 +24,13 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Airplane Detection in Google Maps",
       subtitle: "Computer Vision - Custom CNN",
-      description:
-        "A productivity application for teams to manage projects and tasks.",
+      description: (
+        <>
+          Training a CNN built from scratch on the CIFAR dataset, we are able to recognize airplane's in google images irrespective of the angle or size they are.
+          <br /> <br />
+          Model Accuracy: 86.10%
+        </>
+      ),
       image: "./assets/Airplane.png",
       tags: ["Python", "PyTorch"],
       demo: "#",
@@ -28,8 +40,13 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Crypto Price Predictor",
       subtitle: "Sentiment Analysis | Web Scraping",
-      description:
-        "We perform sentiment analysis on reddit comments about crypto. We merge it with fear and greed index to determine the upcoming crypto price and how close the currencies are intertwined. A comparative study of multiple models.\n\n Models Used: Random Forest Regression, Gradient Boosting Regression, Linear Regression, Random Forest Classification, KNN, Naive Bayes",
+      description: (
+        <>
+          We perform sentiment analysis on reddit comments about crypto. We merge it with fear and greed index to determine the upcoming crypto price and how close the currencies are intertwined. A comparative study of multiple models.
+          <br /><br />
+          Models Used: Random Forest Regression, Gradient Boosting Regression, Linear Regression, Random Forest Classification, KNN, Naive Bayes
+        </>
+      ),
       image: "./assets/crypto.png",
       tags: ["Python", "Seaborn", "Scikit-learn", "Keras", "Spark", "NLTK"],
       demo: "#",
@@ -39,8 +56,11 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Deep Dreamify",
       subtitle: "Digitally Augmented Images - Classification Challenge",
-      description:
-        "A compartive study of multiple models including Support Vector Machines, Decision Trees, Ensemble Voting and Random Forest",
+      description: (
+        <>
+          A comparative study of multiple models including Support Vector Machines, Decision Trees, Ensemble Voting and Random Forest
+        </>
+      ),
       image: "./assets/whatthedogdoin.png",
       tags: ["Python", "Jupyter", "Keras"],
       demo: "#",
@@ -50,8 +70,11 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Emotional Inference - Phoebe from Friends!",
       subtitle: "Emotional Inference Model",
-      description:
-        "We train an AI on the FER2013 database for emotional inference. We then try to predict phoebe's emotions using snapshots from the show F.R.I.E.N.D.S",
+      description: (
+        <>
+          We train an AI on the FER2013 database for emotional inference. We then try to predict phoebe's emotions using snapshots from the show F.R.I.E.N.D.S
+        </>
+      ),
       image: "./assets/phoebe.png",
       tags: ["Python", "Google Colab"],
       demo: "#",
@@ -61,8 +84,11 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Auto-Encoders",
       subtitle: "Dimensionality Reduction | Variation Auto-Encoders",
-      description:
-        "Created a set of autoencoder models, including both vanilla and variational versions, using simple one-layer architectures with ReLU activations. The goal is to explore unsupervised learning by compressing data into a compact representation and then reconstructing it",
+      description: (
+        <>
+          Created a set of autoencoder models, including both vanilla and variational versions, using simple one-layer architectures with ReLU activations. The goal is to explore unsupervised learning by compressing data into a compact representation and then reconstructing it
+        </>
+      ),
       image: "./assets/autoencoder.png",
       tags: ["Python"],
       demo: "#",
@@ -72,8 +98,11 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Data Clustering Unlabelled Data",
       subtitle: "PCA | Dimensionality Reduction | KMeans",
-      description:
-        "Data Clustering of unknown images to identify existing number of classes of data that closely resemble each other.",
+      description: (
+        <>
+          Data Clustering of unknown images to identify existing number of classes of data that closely resemble each other.
+        </>
+      ),
       image: "./assets/cluster.png",
       tags: ["Python"],
       demo: "#",
@@ -83,8 +112,11 @@ const Projects = () => {
       category: "AI/ML Development",
       title: "Handwritten Digit Recognition",
       subtitle: "Convoluted Neural Network",
-      description:
-        "A model to recognize handwritten digits, trained on the MNIST dataset",
+      description: (
+        <>
+          A model to recognize handwritten digits, trained on the MNIST dataset
+        </>
+      ),
       image: "./assets/mnist.jpg",
       tags: ["MATLAB"],
       demo: "#",
@@ -94,8 +126,18 @@ const Projects = () => {
       category: "Mobile Development",
       title: "1Food",
       subtitle: "QR based auto-queuing service for businesses",
-      description:
-        "Features:\n - Pager System (Scan QR ✔) \n - Queue Tracking  \n - Login and Registration / User Profile \n - Nearby Restaurants Info (Google API) \n - Recommendation Engine for Cuisines \n - Favorites \n - Language Support (French & Mandarin)",
+      description: (
+        <>
+          Features:
+          <br /> ● Pager System (Scan QR ✔) 
+          <br /> ● Queue Tracking  
+          <br /> ● Login and Registration / User Profile 
+          <br /> ● Nearby Restaurants Info (Google API) 
+          <br /> ● Recommendation Engine for Cuisines 
+          <br /> ● Favorites 
+          <br /> ● Language Support (French & Mandarin)
+        </>
+      ),
       image: "./assets/1Food.png",
       tags: ["Android", "Firebase", "Kotlin", "Java", "MVVM"],
       demo: "#",
@@ -105,8 +147,11 @@ const Projects = () => {
       category: "Mobile Development",
       title: "Podcasts App",
       subtitle: "An app to manage and view podcasts",
-      description:
-        "The app allows you to browse and view information on available podcasts",
+      description: (
+        <>
+          The app allows you to browse and view information on available podcasts
+        </>
+      ),
       image: "./assets/podcasts.png",
       tags: ["Android", "Kotlin"],
       demo: "#",
@@ -116,8 +161,11 @@ const Projects = () => {
       category: "Mobile Development",
       title: "MineSeeker",
       subtitle: "A mine-seeking game",
-      description:
-        "Within a grid, scan blocks and find all the mines in least number of scans",
+      description: (
+        <>
+          Within a grid, scan blocks and find all the mines in least number of scans
+        </>
+      ),
       image: "./assets/mineseeker.png",
       tags: ["Android", "Java"],
       demo: "#",
@@ -127,8 +175,11 @@ const Projects = () => {
       category: "Mobile Development",
       title: "Game-Over!",
       subtitle: "6 Hour SFU Hackathon",
-      description:
-        "A text based game that lets your traverse the map via text. The tasks involve making decision in certain situations. Making the right decision will allow you to proceed to finish all 5 tasks, a wrong choice, however, restarts the game",
+      description: (
+        <>
+          A text based game that lets your traverse the map via text. The tasks involve making decision in certain situations. Making the right decision will allow you to proceed to finish all 5 tasks, a wrong choice, however, restarts the game
+        </>
+      ),
       image: "./assets/game-over.png",
       tags: ["Android", "Java"],
       demo: "#",
@@ -138,8 +189,11 @@ const Projects = () => {
       category: "Mobile Development",
       title: "My Running Tracker",
       subtitle: "A running tracking app",
-      description:
-        "This app allows users to track and save their running sessions, it is able to automatically identify the activity the user is engaged in using Weka Classifier based on the phone's gyroscopic data",
+      description: (
+        <>
+          This app allows users to track and save their running sessions, it is able to automatically identify the activity the user is engaged in using Weka Classifier based on the phone's gyroscopic data
+        </>
+      ),
       image: "./assets/running_tracker.jpg",
       tags: ["Android", "Kotlin"],
       demo: "#",
@@ -149,8 +203,11 @@ const Projects = () => {
       category: "Web / Backend",
       title: "Hoggett",
       subtitle: "An animal tracking web app",
-      description:
-        "This web application allows a farmer to track his various types of pigs. If he loses a pig, and it is found somewhere else in the city, finders can share the coordinates directly with the farmer on MapBox as a marker location. The list reloads without refetching the page",
+      description: (
+        <>
+          This web application allows a farmer to track his various types of pigs. If he loses a pig, and it is found somewhere else in the city, finders can share the coordinates directly with the farmer on MapBox as a marker location. The list reloads without refetching the page
+        </>
+      ),
       image: "./assets/hogget.png",
       tags: ["TypeScript", "NPM", "Node.js", "MapBox", "MVC", "HTML", "CSS", "Bootstrap 5"],
       demo: "#",
@@ -160,8 +217,11 @@ const Projects = () => {
       category: "Web / Backend",
       title: "Music Industry Management System",
       subtitle: "Relational Database in MSSQL",
-      description:
-        "MusicDB is a MSSQL database application designed to manage the complex relationships in the music industry ecosystem. The system tracks the many-to-many relationships between musicians and artists (bands/groups), maintains song catalogs with proper attribution, and enforces business rules that reflect real-world music industry practices.",
+      description: (
+        <>
+          MusicDB is a MSSQL database application designed to manage the complex relationships in the music industry ecosystem. The system tracks the many-to-many relationships between musicians and artists (bands/groups), maintains song catalogs with proper attribution, and enforces business rules that reflect real-world music industry practices.
+        </>
+      ),
       image: "./assets/database.png",
       tags: ["SQL", "MS SQL Server"],
       demo: "#",
@@ -171,8 +231,11 @@ const Projects = () => {
       category: "Web / Backend",
       title: "Task Manager",
       subtitle: "Java based CRUD app with MongoDB",
-      description:
-        "This app utilizes MongoDB and Spring Boot to build a CRUD app with Database, Backend and Docker containerization",
+      description: (
+        <>
+          This app utilizes MongoDB and Spring Boot to build a CRUD app with Database, Backend and Docker containerization
+        </>
+      ),
       image: "",
       tags: ["Java", "SpringBoot", "MongoDB", "Docker"],
       demo: "#",
@@ -182,8 +245,14 @@ const Projects = () => {
       category: "Systems / Low Level",
       title: "Reconstructing 3D from 2D source",
       subtitle: "Stereo Reconstruction",
-      description:
-        "No Libraries, only pure math! \n Theory: https://w.wiki/DQ8e \n\n I perform some tricks to get the ability to transform an airplane from a 2D image to a 3D representation. (Understanding the \"depth\" of the 2D image. Pun intended)",
+      description: (
+        <>
+          No Libraries, only pure math! 
+          <br /> Theory: https://w.wiki/DQ8e 
+          <br /><br />
+          I perform some tricks to get the ability to transform an airplane from a 2D image to a 3D representation. (Understanding the "depth" of the 2D image. Pun intended)
+        </>
+      ),
       image: "./assets/stereo_reconstruction.jpeg",
       tags: ["MATLAB"],
       demo: "#",
@@ -193,8 +262,13 @@ const Projects = () => {
       category: "Systems / Low Level",
       title: "Augmented Reality Filter",
       subtitle: "Real-time Superimposition",
-      description:
-        "No libraries were used, only pure math! \n\nWe take a video with constantly changing perspectives, learning where the edge's for the book are and what are it's dimensions to successfully impose a live video over it.",
+      description: (
+        <>
+          No libraries were used, only pure math! 
+          <br /><br />
+          We take a video with constantly changing perspectives, learning where the edge's for the book are and what are it's dimensions to successfully impose a live video over it.
+        </>
+      ),
       image: "https://user-images.githubusercontent.com/70075553/180334483-7aa4f375-1e79-49ed-a960-652f46076de6.mp4",
       tags: ["MATLAB", "Brief", "Surf", "RANSAC", "Image Stitching"],
       demo: "#",
@@ -204,8 +278,11 @@ const Projects = () => {
       category: "Systems / Low Level",
       title: "Multi-Threaded UDP Chat",
       subtitle: "Implementation for Real-Time Messaging over IPv4",
-      description:
-        "communicate over a network using UDP. It employs multiple threads to handle simultaneous tasks—reading user input, sending messages, receiving messages, and displaying received messages. The program also integrates a simple encryption mechanism and custom list data structures to manage message buffers, showcasing an effective combination of socket programming, threading, and data structure manipulation.",
+      description: (
+        <>
+          Communicate over a network using UDP. It employs multiple threads to handle simultaneous tasks—reading user input, sending messages, receiving messages, and displaying received messages. The program also integrates a simple encryption mechanism and custom list data structures to manage message buffers, showcasing an effective combination of socket programming, threading, and data structure manipulation.
+        </>
+      ),
       image: "./assets/udp.png",
       tags: ["C"],
       demo: "#",
@@ -215,8 +292,11 @@ const Projects = () => {
       category: "Systems / Low Level",
       title: "Custom 'ls' Command Implementation",
       subtitle: "Filesystem Navigation and Inode Exploration",
-      description:
-        "A custom re-creation of the classic Linux ls command, built using C. It showcases expertise in low-level filesystem operations by reading inode data, traversing directories recursively, and sorting directory entries. The application supports several command-line options for enhanced functionality",
+      description: (
+        <>
+          A custom re-creation of the classic Linux ls command, built using C. It showcases expertise in low-level filesystem operations by reading inode data, traversing directories recursively, and sorting directory entries. The application supports several command-line options for enhanced functionality.
+        </>
+      ),
       image: "",
       tags: ["C"],
       demo: "#",
@@ -226,8 +306,11 @@ const Projects = () => {
       category: "Systems / Low Level",
       title: "PID Traversal",
       subtitle: "System Process and Resource Analyzer",
-      description:
-        "A set of three concise C programs that traverse the /proc filesystem. One module extracts and displays PIDs and their command names, another computes memory utilization from /proc/meminfo, and the third retrieves kernel and OS version details.",
+      description: (
+        <>
+          A set of three concise C programs that traverse the /proc filesystem. One module extracts and displays PIDs and their command names, another computes memory utilization from /proc/meminfo, and the third retrieves kernel and OS version details.
+        </>
+      ),
       image: "",
       tags: ["C"],
       demo: "#",
@@ -264,7 +347,7 @@ const Projects = () => {
   return (
     <div class="container my-5">
       <div class="text-center mb-5">
-        <h2>My Projects</h2>
+        <h2>Software I've Built</h2>
       </div>
 
       <div class="nav-grid text-center mb-5 justify-content-center align-items-center">
@@ -300,12 +383,7 @@ const Projects = () => {
                     <small>{project.subtitle}</small>
                   </h6>
                   <p class="card-text flex-grow-3">
-                    {project.description.split("\n").map((line) => (
-                      <>
-                        {line}
-                        <br />
-                      </>
-                    ))}
+                    {project.description}
                   </p>
                   <div class="d-flex flex-wrap gap-2 mb-3">
                     <For each={project.tags}>
