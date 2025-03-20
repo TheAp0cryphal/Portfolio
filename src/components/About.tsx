494979477077e33c1bcb6d4364fca5fc2ec30f9f
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-19 20:19:22 -0700

    WIP: About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,10 +4,70 @@ const About = () => {
       <div class="text-center mb-5">
         <h2>About Me</h2>
       </div>
-      <div class="row align-items-center"/>
+      <div class="row">
+        <div class="col-md-6">
+          <h5 class="mb-3">Congratulations for making it till here! That tells me you're curious, which is a trait I highly value (and probably share!). 
+            I feel this a great opportunity for me to broadcast about myself, how I think and my world-view of the current state of affairs in technology and how I reflect on it.
+          </h5>
+          <p>
+            I like working with, and spend most of my time fiddling with computers. To nurture a holistic understanding of technology and business,
+            I keep up-to-date with the latest developments, observing the rapid progress that's happening each day. 
+          </p>
+          <h5>For some people, programming is simply a zero-sum algorithm game...</h5>
+          <p> 
+            I beg to differ, I believe that the real skill lies in having a comprehensive vision for how these cogs fit together
+             to build a functional and valuable system; which is what Software Engineering truly entails. 
+             My deep appreciation for this broader perspective, gained through building complex software, has driven me to cultivate my acumen in this area.
+          </p>
+          <p>
+          My skills as a software engineer are complemented by a clear technological vision to prioritize high impact tasks successfully.
+          I believe a crucial aspect of technical expertise is knowing when not to over-engineer solutions, 
+          understanding the limitations and avoiding the computational equivalent of using a sledgehammer to crack a nut.
+          </p>
+          <ul>
+            <li>Point 1: Looks good</li>
+            <li>Point 2: Reads well</li>
+            <li>Point 3: Adds visual interest</li>
+          </ul>
+        </div>
+        <div class="col-md-6">
+          <h3>
+         .--,_
+        ['    '\.
+         \       `''|
+         |         ,]
+          `._      ].
+            |     \
+          _/       -'\
+         ,'          ,'
+       _/'          \                     ,..-''L_
+  |--''              '-;__        |\     /      .,'
+   \                      `--.__,'_ '----     ,-'
+   `\                             \`-'\__    ,|
+,--;/                             /     .| ,/
+\__                               '|    /  / 
+  ./  _-,                         _| 
+  \__/ /                        ,/        "
+       |                      _/
+       |                    ,/
+       \                   /
+        |              /.-'
+         \           _/                   :
+          |         /                      .
+           |        |                     .
+      .    |        |                     '.
+      ;     \       /                     ;\
+      '      |     |                
+             \    _|                      : 
+              \_,/                         "'
+                                          : '
+                                             '
+</h3>
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default About;
   
