7cc6bed3be57b94a6763ebec5c0babcf07de47e2
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-28 17:12:50 -0700

    Setting up light switch game

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,55 +1,78 @@
+import { createSignal } from "solid-js";
+
 const About = () => {
+  const [showAbout, setShowAbout] = createSignal(false);
+
   return (
     <div class="container">
       <div class="text-center mb-5">
         <h2>About Me</h2>
       </div>
-      <div class="row">
-        <div class="col-md-6">
-          <h4><span class="marked-text ">Hey? You, yes you! Finally, you're here!</span></h4>
-          <p class="h5">Congratulations for making it till here! That tells me you're curious, which is a trait I highly value (and probably share!). 
-            This is a great opportunity for me to broadcast about myself, how I think and my world-view of the current state of affairs in technology and how I reflect on it.
-          </p>
-          <p>
-            I like working with, and spend most of my time fiddling with technology. To nurture a holistic understanding of tech and business,
-            I keep up-to-date with the latest developments, observing and building upon the rapid progress that's happening each day. 
-          </p>
-          <p>
-            I am a <span class="marked-text">π-shaped software engineer</span> with deep vertical expertise in Mobile Development, Backend Systems, and AI/ML. 
-            My breadth spans embedded systems, full-stack development, and UI/UX design. I've had the chance to work on everything from building 
-            vehicle infotainment platforms at Rivian to creating AI-powered speech recognition and generative tools for a gaming startup. 
-            Along the way, I've engineered backend solutions using Python and Azure, and developed 
-            robust mobile apps in Kotlin and Java that have reached thousands of users.
-          </p>
-          <p></p>
-        </div>
-        <div class="col-md-6">
-          <p>The need to have a multi-faceted approach to creating valuable software is something that is becoming extremely necessary.
-            This is why I've built a diverse skillset that allows me to attack the problem on all sides and provide a unique value proposition.
-          </p>
-          <h4>Beyond Programming: The Art of Software Engineering</h4>
-          <p class="h5">For some people, programming is simply a zero-sum algorithm game...</p>
-            <p> 
-              I beg to differ, I believe that the real skill lies in having a comprehensive vision for how these cogs fit together
-              to build a functional and valuable system; which is what Software Engineering truly entails. 
-              My deep appreciation for this broader perspective, gained through building complex software, has driven me to cultivate my acumen in this area.
-            </p>
-            <p>
-            My skills as a software engineer are complemented by a clear technological vision to prioritize high impact tasks successfully.
-            I believe a crucial aspect of technical expertise is knowing when not to over-engineer solutions, 
-            understanding the limitations and avoiding the computational equivalent of using a sledgehammer to crack a nut.
-            </p>
-            <p class="h5">I like to evaluate my work on the following principles:</p>
-            <ul> 
-              <li><span class="marked-text">User First:</span> I always begin by empathizing with the end-user, ensuring every decision is rooted in genuine user needs and real-world challenges.</li>
-              <li><span class="marked-text">Elegant Simplicity:</span> I believe that clarity is the foundation of great design; each system should be as straightforward as it is functional.</li>
-              <li><span class="marked-text">Scalable Efficiency:</span> My approach centers on maintaining peak performance, with a constant focus on reducing bottlenecks and latency.</li>
-            </ul>
-        </div>
+
+
+
+      
+      <button onClick={() => setShowAbout(!showAbout())}>
+        {showAbout() ? "Hide About Me" : "Show About Me"}
+      </button>
+      
+      <div class={`about-content ${showAbout() ? 'fade-in' : 'fade-out'}`}>
+        {showAbout() && (
+          <div class="row">
+            <div class="col-md-6">
+              <p class="h5">
+                Congratulations for making it till here! That tells me you're curious, which is a trait I highly value (and probably share!). 
+                This is a great opportunity for me to broadcast about myself, how I think and my world-view of the current state of affairs in technology and how I reflect on it.
+              </p>
+              <p>
+                I like working with, and spend most of my time fiddling with technology. To nurture a holistic understanding of tech and business,
+                I keep up-to-date with the latest developments, observing and building upon the rapid progress that's happening each day. 
+              </p>
+              <p>
+                I am a <span class="marked-text">π-shaped software engineer</span> with deep vertical expertise in Mobile Development, Backend Systems, and AI/ML. 
+                My breadth spans embedded systems, full-stack development, and UI/UX design. I've had the chance to work on everything from building 
+                vehicle infotainment platforms at Rivian to creating AI-powered speech recognition and generative tools for a gaming startup. 
+                Along the way, I've engineered backend solutions using Python and Azure, and developed 
+                robust mobile apps in Kotlin and Java that have reached thousands of users.
+              </p>
+            </div>
+            <div class="col-md-6">
+              <p>
+                The need to have a multi-faceted approach to creating valuable software is something that is becoming extremely necessary.
+                This is why I've built a diverse skillset that allows me to attack the problem on all sides and provide a unique value proposition.
+              </p>
+              <h4>Beyond Programming: The Art of Software Engineering</h4>
+              <p class="h5">
+                For some people, programming is simply a zero-sum algorithm game...
+              </p>
+              <p>
+                I beg to differ, I believe that the real skill lies in having a comprehensive vision for how these cogs fit together
+                to build a functional and valuable system; which is what Software Engineering truly entails. 
+                My deep appreciation for this broader perspective, gained through building complex software, has driven me to cultivate my acumen in this area.
+              </p>
+              <p>
+                My skills as a software engineer are complemented by a clear technological vision to prioritize high impact tasks successfully.
+                I believe a crucial aspect of technical expertise is knowing when not to over-engineer solutions, 
+                understanding the limitations and avoiding the computational equivalent of using a sledgehammer to crack a nut.
+              </p>
+              <p class="h5">I like to evaluate my work on the following principles:</p>
+              <ul> 
+                <li>
+                  <span class="marked-text">User First:</span> I always begin by empathizing with the end-user, ensuring every decision is rooted in genuine user needs and real-world challenges.
+                </li>
+                <li>
+                  <span class="marked-text">Elegant Simplicity:</span> I believe that clarity is the foundation of great design; each system should be as straightforward as it is functional.
+                </li>
+                <li>
+                  <span class="marked-text">Scalable Efficiency:</span> My approach centers on maintaining peak performance, with a constant focus on reducing bottlenecks and latency.
+                </li>
+              </ul>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
-}
+};
 
 export default About;
-  
