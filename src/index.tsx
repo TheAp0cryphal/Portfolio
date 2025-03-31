efe49c9e283b2abaeabd2117a273dd8d02c7a5bd
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-31 15:08:29 -0700

    vercel analytics test

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import { render } from "solid-js/web";
 import "./cheatsheet.scss";
 import App from "./App";
-
+import { inject } from '@vercel/analytics';
+ 
+inject();
 render(() => <App />, document.getElementById("root")!);
