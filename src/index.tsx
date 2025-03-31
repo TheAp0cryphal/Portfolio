0a24697ca90197bea005c959a1cd3a1fe5034efb
TheAp0cryphal <tanishk.sh@proton.me>
2025-03-31 15:06:08 -0700

    Revert "Added Vercel Analytics"
    This reverts commit 157dfdbae5b3ac8887ab8ba4f0d2d988765af118.

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,5 @@
 import { render } from "solid-js/web";
 import "./cheatsheet.scss";
 import App from "./App";
-import { Analytics } from "@vercel/analytics/react";
 
-render(() => (
-  <>
-    <App />
-    <Analytics />
-  </>
-), document.getElementById("root")!);
+render(() => <App />, document.getElementById("root")!);
