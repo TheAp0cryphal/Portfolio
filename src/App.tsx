e1f1cfb4acf63545ee6807aca0dcfd019543a9b6
TheAp0cryphal <tanishk.sh@proton.me>
2025-06-02 19:46:04 -0700

    minor center fix

--- src/App.tsx
+++ src/App.tsx
@@ -14,41 +14,41 @@ const App = () => {
       <div class="gradient-background">
         <header>
           <nav class="navbar navbar-expand-lg fixed-top">
-            <div class="container">
-              <a class="navbar-brand " href="#home">
+            <div class="container d-flex justify-content-between align-items-center">
+              <a class="navbar-brand" href="#home">
                 <span class="navbar-name">T<span class="hide-effect">anishk</span></span>
               </a>
-                <ul class="navbar-nav d-none d-lg-flex">
-                  <li class="nav-item">
-                    <a class="nav-link" href="#home">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#about">About</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#experience">Experience</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link" href="#projects">Projects</a>
-                  </li>
-                </ul>
-                <ul class="navbar-nav d-flex flex-row">
-                  <li>
-                    <a class="nav-icon" href="https://www.github.com/TheAp0cryphal" data-bs-toggle="tooltip" title="GitHub">
-                      <AiFillGithub />
-                    </a>
-                  </li>
-                  <li>
-                    <a class="nav-icon" href="https://www.linkedin.com/in/tanishks" data-bs-toggle="tooltip" title="LinkedIn">
-                      <AiFillLinkedIn />
-                    </a>
-                  </li>
-                  <li>
-                    <a class="nav-icon" href="mailto:tanishk.sh@proton.me" data-bs-toggle="tooltip" title="mail: tanishk.sh@proton.me">
-                      <AiFillMailIcon />
-                    </a>
-                  </li>
-                </ul>
+              <ul class="navbar-nav d-none d-lg-flex flex-row">
+                <li class="nav-item">
+                  <a class="nav-link" href="#home">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#experience">Experience</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#projects">Projects</a>
+                </li>
+              </ul>
+              <ul class="navbar-nav d-flex flex-row">
+                <li>
+                  <a class="nav-icon" href="https://www.github.com/TheAp0cryphal" data-bs-toggle="tooltip" title="GitHub">
+                    <AiFillGithub />
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-icon" href="https://www.linkedin.com/in/tanishks" data-bs-toggle="tooltip" title="LinkedIn">
+                    <AiFillLinkedIn />
+                  </a>
+                </li>
+                <li>
+                  <a class="nav-icon" href="mailto:tanishk.sh@proton.me" data-bs-toggle="tooltip" title="mail: tanishk.sh@proton.me">
+                    <AiFillMailIcon />
+                  </a>
+                </li>
+              </ul>
             </div>
           </nav>
         </header>
